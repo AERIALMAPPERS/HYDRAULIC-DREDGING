06524f6ee8df4336dc141ab3d32bd99dabaf704d
AERIALMAPPERS <149242949+AERIALMAPPERS@users.noreply.github.com>
2024-10-10 12:27:05 +0530

    service section

--- assets/js/service.js
+++ assets/js/service.js
@@ -1,34 +1,37 @@
-let currentIndex = 1;
-const totalServices = 3;
 
-function showService(index) {
-    // Hide all services
-    const services = document.querySelectorAll('.service');
-    services.forEach((service) => {
-        service.classList.remove('active');
-    });
+        let currentIndex = 1;
+        const totalServices = 4;
 
-    // Remove active class from all dots
-    const dots = document.querySelectorAll('.dot');
-    dots.forEach((dot) => {
-        dot.classList.remove('active');
-    });
+        function showService(index) {
+            // Hide all services
+            const services = document.querySelectorAll('.service');
+            services.forEach((service) => {
+                service.classList.remove('active');
+            });
 
-    // Calculate the new index (looping back to the start if necessary)
-    currentIndex = index > totalServices ? 1 : index < 1 ? totalServices : index;
+            // Remove active class from all dots
+            const dots = document.querySelectorAll('.dot');
+            dots.forEach((dot) => {
+                dot.classList.remove('active');
+            });
 
-    // Show the selected service and activate the corresponding dot
-    document.getElementById(`service${currentIndex}`).classList.add('active');
-    dots[currentIndex - 1].classList.add('active');
-}
+            // Calculate the new index (looping back to the start if necessary)
+            currentIndex = index > totalServices ? 1 : index < 1 ? totalServices : index;
 
-function nextService() {
-    currentIndex++;
-    showService(currentIndex);
-}
+            // Show the selected service and activate the corresponding dot
+            document.getElementById(`service${currentIndex}`).classList.add('active');
+            dots[currentIndex - 1].classList.add('active');
+        }
 
-// Set an interval to automatically change Services every 5 seconds
-setInterval(nextService, 3000);
+        function nextService() {
+            currentIndex++;
+            showService(currentIndex);
+        }
 
-// Optional: Add swipe functionality for touch devices
-// You can implement this using additional JavaScript or libraries
+        function prevService() {
+            currentIndex--;
+            showService(currentIndex);
+        }
+
+        // Set an interval to automatically change services every 5 seconds
+        // setInterval(nextService, 5000);
